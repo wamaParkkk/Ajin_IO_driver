4af2bd7e0fbfef9776a3a7dacb474fdb72be718a
Hyunsoo Park <zahir84@nate.com>
2025-06-03 17:30:45 +0900

    25/06/03 update2

--- Ajin_IO_driver/DIOClass.cs
+++ Ajin_IO_driver/DIOClass.cs
@@ -188,8 +188,7 @@ namespace Ajin_IO_driver
 				{
 					case AXT_MODULE.AXT_SIO_DO32P:
 					case AXT_MODULE.AXT_SIO_DO32T:
-					case AXT_MODULE.AXT_SIO_RDO32:
-					case AXT_MODULE.AXT_SIO_DB32P:
+					case AXT_MODULE.AXT_SIO_RDO32:					
 						CAXD.AxdoWriteOutportBit(iModuleID + 1, nIndex, uValue);
 						break;
 
@@ -219,8 +218,7 @@ namespace Ajin_IO_driver
 				{
 					case AXT_MODULE.AXT_SIO_DO32P:
 					case AXT_MODULE.AXT_SIO_DO32T:
-					case AXT_MODULE.AXT_SIO_RDO32:
-                    case AXT_MODULE.AXT_SIO_DB32P:
+					case AXT_MODULE.AXT_SIO_RDO32:                    
                         CAXD.AxdoWriteOutportBit(iModuleID + 2, nIndex - 32, uValue);
 						break;
 
